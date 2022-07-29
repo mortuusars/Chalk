dfe7e19bd558ad458f3c36430c48dae726371d88
mortuusars <mortuusars@gmail.com>
2022-07-30 00:42:11 +0300

    Chalk Box recipe.

--- src/main/java/io/github/mortuusars/chalk/data/ModRecipeProvider.java
+++ src/main/java/io/github/mortuusars/chalk/data/ModRecipeProvider.java
@@ -7,12 +7,14 @@ import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.registries.ForgeRegistries;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 import java.util.function.Consumer;
@@ -21,11 +23,9 @@ public class ModRecipeProvider extends RecipeProvider {
     public ModRecipeProvider(DataGenerator generator) { super(generator); }
 
     @Override
-    protected void buildCraftingRecipes(Consumer<FinishedRecipe> recipeBuilder) {
+    protected void buildCraftingRecipes(@NotNull Consumer<FinishedRecipe> recipeBuilder) {
         ModItems.CHALKS.forEach( (name, item) -> {
-
             DyeColor color = item.get().getColor();
-
             ShapelessRecipeBuilder.shapeless(item.get(), 1)
                     .unlockedBy("has_calcite", has(Items.CALCITE))
                     .group("chalk:chalk")
@@ -33,5 +33,14 @@ public class ModRecipeProvider extends RecipeProvider {
                     .requires(Objects.requireNonNull(ForgeRegistries.ITEMS.getValue(new ResourceLocation(color + "_dye"))).asItem())
                     .save(recipeBuilder, Chalk.MOD_ID + ":chalk_from_" + color + "_dye");
         });
+
+        ShapedRecipeBuilder.shaped(ModItems.CHALK_BOX.get())
+                .unlockedBy("has_chalk", has(ModTags.Items.CHALK))
+                .unlockedBy("has_paper", has(Items.PAPER))
+                .pattern("P P")
+                .pattern("P P")
+                .pattern("PPP")
+                .define('P', Items.PAPER)
+                .save(recipeBuilder);
     }
 }
