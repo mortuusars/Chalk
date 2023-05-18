931ee31b2067c46ea57d78b5c7830dfe1b40c586
mortuusars <mortuusars@gmail.com>
2023-05-18 03:51:56 +0300

    Horizontal orientation based on player look direction.

--- src/main/java/io/github/mortuusars/chalk/network/packet/ServerboundOpenChalkBoxPacket.java
+++ src/main/java/io/github/mortuusars/chalk/network/packet/ServerboundOpenChalkBoxPacket.java
@@ -2,7 +2,9 @@ package io.github.mortuusars.chalk.network.packet;
 
 import io.github.mortuusars.chalk.Chalk;
 import io.github.mortuusars.chalk.items.ChalkBoxItem;
+import net.minecraft.advancements.Advancement;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.PlayerAdvancements;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.network.NetworkEvent;
